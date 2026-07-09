9614b3bd853ded688b2457defc22c537d895610a
Shivanjali Dange <shivanjali.s.dange@avyka.com>
2026-07-09 02:35:49 +0000

    Create POC1/varibales.tf

---
+++ POC1/varibales.tf
@@ -0,0 +1,21 @@
+#Inputs your code expect
+
+variable "bucket_name" {
+  type        = string
+  description = "Name of the S3 bucket"
+}
+
+variable "aws_region" {
+  description = "AWS Region"
+  type        = string
+  default     = "us-east-1"
+}
+
+variable "tags" {
+  description = "Tags"
+  type        = map(string)
+  default     = {
+    Environment = "Dev"
+    ManagedBy   = "Harness"
+  }
+}
