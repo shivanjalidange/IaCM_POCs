7a4b3f7389d94d3e47658a5fc589aeaa6ed757fe
Shivanjali Dange <shivanjali.s.dange@avyka.com>
2026-07-16 06:06:44 +0000

    Edit POC2/main.tf

--- POC2/main.tf
+++ POC2/main.tf
@@ -1,9 +1,14 @@
 resource "azurerm_storage_account" "res-0" {
-  account_replication_type        = "LRS"
-  account_tier                    = "Standard"
-  allow_nested_items_to_be_public = false
-  location                        = "eastus"
-  min_tls_version                 = "TLS1_0"
-  name                            = "shivaiacmpoc2"
-  resource_group_name             = "rg-avyka-users-temp"
+
+  name                     = var.storage_account_name
+  resource_group_name      = var.resource_group_name
+  location                 = var.location
+
+  account_tier             = var.account_tier
+  account_replication_type = var.account_replication_type
+
+  tags = {
+    Environment = var.environment
+    Owner       = var.owner
+  }
 }
