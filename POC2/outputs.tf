2f688ecd0037be38b70f2e1af6cbe6584466f9bf
Shivanjali Dange <shivanjali.s.dange@avyka.com>
2026-07-16 06:31:43 +0000

    Create POC2/outputs.tf

---
+++ POC2/outputs.tf
@@ -0,0 +1,15 @@
+output "storage_account_name" {
+  value = azurerm_storage_account.res-0.name
+}
+
+output "storage_account_id" {
+  value = azurerm_storage_account.res-0.id
+}
+
+output "resource_group_name" {
+  value = azurerm_storage_account.res-0.resource_group_name
+}
+
+output "primary_blob_endpoint" {
+  value = azurerm_storage_account.res-0.primary_blob_endpoint
+}
