6b3dce10d39222810e7124325501d586cb4618fc
shivanjalidange <147036982+shivanjalidange@users.noreply.github.com>
2026-07-24 18:24:34 +0530

    Update provider.tf

--- POC3/terraform/provider.tf
+++ POC3/terraform/provider.tf
@@ -1,12 +1,14 @@
 terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 6.0"
-    }
-  }
+  required_version = ">= 1.5.0"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 6.0"
+    }
+  }
 }
 
 provider "aws" {
-  region = "us-east-1"
+  region = "us-east-1"
 }
