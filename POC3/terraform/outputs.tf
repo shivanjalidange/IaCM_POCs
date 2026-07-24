892d8f913803e345e491949a45dd64e0bdfd8234
shivanjalidange <147036982+shivanjalidange@users.noreply.github.com>
2026-07-24 18:25:42 +0530

    Update outputs.tf

--- POC3/terraform/outputs.tf
+++ POC3/terraform/outputs.tf
@@ -1,11 +1,14 @@
 output "vpc_id" {
-  value = aws_vpc.main.id
+  value       = aws_vpc.main.id
+  description = "The ID of the migrated VPC"
 }
 
 output "subnet_id" {
-  value = aws_subnet.public.id
+  value       = aws_subnet.public.id
+  description = "The ID of the public subnet"
 }
 
 output "security_group_id" {
-  value = aws_security_group.web.id
+  value       = aws_security_group.web.id
+  description = "The ID of the web security group"
 }
