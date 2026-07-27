0806c37020332b71b846d728c9ee499cab8cdef1
shivanjalidange <147036982+shivanjalidange@users.noreply.github.com>
2026-07-27 12:52:31 +0530

    Create import.tf

---
+++ POC3/terraform/import.tf
@@ -0,0 +1,14 @@
+import {
+  to = aws_vpc.main
+  id = "vpc-0b83ef2580e3f332" 
+}
+
+import {
+  to = aws_subnet.public
+  id = "subnet-0a52d86b77c09ac6" # Replace with your deployed Subnet ID
+}
+
+import {
+  to = aws_security_group.web
+  id = "sg-042245d85b6378577" # Replace with your deployed Security Group ID
+}
